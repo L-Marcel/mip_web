53d320618efc251779370b3fee894501027d76ca
C-Fernandes <66803310+C-Fernandes@users.noreply.github.com>
2021-07-20 19:02:58 -0300

    Estilização da listagem de produtos

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -4,12 +4,10 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 export default function Menu() {
     return (
 
-
-
-        <Navbar bg="dark" variant="dark" className="nav">
+        <Navbar bg="dark" variant="dark" >
             <Container>
-                <Navbar.Brand href="/"><p>Macau</p>
-                    <h4><span className="danger">Independent</span> Points</h4></Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <h1>M<span className="danger">I</span>P</h1></Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="/">Inicio</Nav.Link>
                     <Nav.Link href="http://localhost:3000/products">Produtos</Nav.Link>
--- src/pages/product/List.tsx
+++ src/pages/product/List.tsx
@@ -2,102 +2,116 @@ import React, { useEffect, useState } from 'react';
 import Menu from '../../components/Nav';
 import connection from '../../services/connection';
 import ProductModal from './Modal';
+import { Button, ListGroup, Table } from 'react-bootstrap';
 
 export default function ProductListPage() {
- const [products, setProducts] = useState<Product[]>([]);
- const [modalProps, setModalProps] = useState<ProductModalProps>({
-  show: false,
-  defaultProduct: {
-   job: -1,
-   delivery: false,
-   name: "",
-   type: "Item",
-   price: 0,
-   unit: 1,
-  }
- });
-
- useEffect(() => {
-  handleUpdateList();
- }, []);
-
- //Atualiza a lista de produtos
- async function handleUpdateList() {
-  await connection.get('products')
-   .then((res) => {
-    setProducts(res.data);
-   })
-   .catch(() => { });
- };
-
- async function callEditModal(p: Product) {
-  setModalProps({
-   defaultProduct: p,
-   show: true,
-  });
- };
-
- async function deleteProduct(p: Product) {
-  await connection.delete(`products/delete?id=${p.id}`)
-  .then(() => {
-   handleUpdateList();
-  })
-  .catch(() => {});
- };
-
- function handleShowModal() {
-  setModalProps({
-   defaultProduct: {
-    job: -1,
-    delivery: false,
-    name: "",
-    type: "Item",
-    price: 0,
-    unit: 1,
-   },
-   show: true,
-  });
- };
-
- function handleHideModal() {
-  setModalProps({
-   ...modalProps,
-   show: false,
-  });
- };
-
- return (
-  <div>
-    <Menu/>
-  
-   <ProductModal
-    {...modalProps}
-    onClose={handleHideModal}
-    onFinish={async (p) => {
-     //Se tiver o ID, quer dizer que é para alterar...
-
-     if (p.id === undefined) {
-      await connection.post('/products/create', p).then(() => { }).catch(() => { });
-     } else {
-      await connection.post('/products/update', p).then(() => { }).catch(() => { });
-     }
-     handleUpdateList();
-    }}
-   />
-   <button onClick={handleShowModal}>Adicionar novo produto</button>
-   <ul>
-    {
-     products.map((p, i) => {
-      return (
-       <li key={`products-${i}`}>
-        <h3>{p.name} - R$ {p.price.toFixed(2)}</h3>
-        <button onClick={() => callEditModal(p)}>Editar</button>
-        <button onClick={() => deleteProduct(p)}>Excluir</button>
-       </li>
-      );
-     })
+  const [products, setProducts] = useState<Product[]>([]);
+  const [modalProps, setModalProps] = useState<ProductModalProps>({
+    show: false,
+    defaultProduct: {
+      job: -1,
+      delivery: false,
+      name: "",
+      type: "Item",
+      price: 0,
+      unit: 1,
     }
-   </ul>
-  </div>
- );
+  });
+
+  useEffect(() => {
+    handleUpdateList();
+  }, []);
+
+  //Atualiza a lista de produtos
+  async function handleUpdateList() {
+    await connection.get('products')
+      .then((res) => {
+        setProducts(res.data);
+      })
+      .catch(() => { });
+  };
+
+  async function callEditModal(p: Product) {
+    setModalProps({
+      defaultProduct: p,
+      show: true,
+    });
+  };
+
+  async function deleteProduct(p: Product) {
+    await connection.delete(`products/delete?id=${p.id}`)
+      .then(() => {
+        handleUpdateList();
+      })
+      .catch(() => { });
+  };
+
+  function handleShowModal() {
+    setModalProps({
+      defaultProduct: {
+        job: -1,
+        delivery: false,
+        name: "",
+        type: "Item",
+        price: 0,
+        unit: 1,
+      },
+      show: true,
+    });
+  };
+
+  function handleHideModal() {
+    setModalProps({
+      ...modalProps,
+      show: false,
+    });
+  };
+
+  return (
+    <div>
+      <Menu />
+
+      <ProductModal
+        {...modalProps}
+        onClose={handleHideModal}
+        onFinish={async (p) => {
+          //Se tiver o ID, quer dizer que é para alterar...
+
+          if (p.id === undefined) {
+            await connection.post('/products/create', p).then(() => { }).catch(() => { });
+          } else {
+            await connection.post('/products/update', p).then(() => { }).catch(() => { });
+          }
+          handleUpdateList();
+        }}
+      />
+      <div className="styleList">
+        <ul>
+          {
+            products.map((p, i) => {
+              return (
+                <Table size="sm" >
+
+                  <thead>
+                    <tr key={`products-${i}`}>
+                      <td ><h3>{p.name} - R$ {p.price.toFixed(2)}</h3></td>
+                      <td></td>
+                      <td>
+                        <Button variant="danger" onClick={() => deleteProduct(p)} >Excluir</Button>
+                        <Button variant="secondary" onClick={() => callEditModal(p)} >Editar</Button>
+                      </td>
+                    </tr>
+                  </thead>
+
+                </Table>
+
+              );
+            })
+          }
+
+        </ul>
+        <Button onClick={handleShowModal} variant="danger">Adicionar novo produto</Button>
+      </div>
+    </div>
+  );
 };
